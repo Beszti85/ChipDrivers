1fcfa968965cd0db6d9f7fe3f2cc521ceee4436e
Beszti85 <51108540+Beszti85@users.noreply.github.com>
2022-06-01 08:28:34 +0200

    [FLS]: added initial driver for Winbond W25 flash series

---
+++ Inc/flash_w25.h
@@ -0,0 +1,10 @@
+/*
+ * flash_w25.h
+ *
+ *  Created on: 2022. jún. 1.
+ *      Author: drCsabesz
+ */
+
+extern SPI_HandleTypeDef hspi1;
+
+void FLASH_W25_Identification( void );
---
+++ Src/flash_w25.c
@@ -0,0 +1,20 @@
+/*
+ * flash_w25.c
+ *
+ *  Created on: 2022. jún. 1.
+ *      Author: drCsabesz
+ */
+#include "stm32f4xx_hal.h"
+#include "main.h"
+#include "flash_w25.h"
+
+uint8_t FLASH_TxBuffer[32u];
+uint8_t FLASH_RxBuffer[32u];
+
+void FLASH_W25_Identification( void )
+{
+  // Read Jedec ID
+  FLASH_TxBuffer[0] = 0x9Fu;
+  HAL_SPI_TransmitReceive(&hspi1, FLASH_TxBuffer, FLASH_RxBuffer, 4u, 100u);
+  HAL_GPIO_WritePin(CS_FLASH_GPIO_Port, CS_FLASH_Pin, GPIO_PIN_SET);
+}
