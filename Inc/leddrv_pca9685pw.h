b4b126fbfb515ddaceac045158f31524ab4fe083
Beszti85 <51108540+Beszti85@users.noreply.github.com>
2022-06-02 17:08:48 +0200

    Initial files for leddriver PCA9685PW

---
+++ Inc/leddrv_pca9685pw.h
@@ -0,0 +1,13 @@
+/*
+ * leddrv_pca9685pw.h
+ *
+ *  Created on: 2022. jún. 2.
+ *      Author: drCsabesz
+ */
+
+#ifndef INC_LEDDRV_PCA9685PW_H_
+#define INC_LEDDRV_PCA9685PW_H_
+
+
+
+#endif /* INC_LEDDRV_PCA9685PW_H_ */
---
+++ Src/leddrv_pca9685pw.c
@@ -0,0 +1,15 @@
+/*
+ * leddrv_pca9685pw.c
+ *
+ *  Created on: 2022. jún. 2.
+ *      Author: drCsabesz
+ */
+
+#include "stm32f4xx_hal.h"
+#include "main.h"
+#include "leddrv_pca9685pw.h"
+
+void LEDDRV_PCA9685_ReadModeRegs( void )
+{
+
+}
