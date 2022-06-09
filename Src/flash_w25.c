03338c421fed85aaeeea2e59ad0f97c03388c319
Beszti85 <51108540+Beszti85@users.noreply.github.com>
2022-06-09 23:03:04 +0200

    [FLASH]: detect winbond W25 flash

--- Src/flash_w25.c
+++ Src/flash_w25.c
@@ -10,6 +10,7 @@
 
 uint8_t FLASH_TxBuffer[32u];
 uint8_t FLASH_RxBuffer[32u];
+uint8_t FLASH_Detected = 0u;
 
 extern SPI_HandleTypeDef hspi1;
 
@@ -20,6 +21,11 @@ void FLASH_W25_Identification( void )
   HAL_GPIO_WritePin(CS_FLASH_GPIO_Port, CS_FLASH_Pin, GPIO_PIN_RESET);
   HAL_SPI_TransmitReceive(&hspi1, FLASH_TxBuffer, FLASH_RxBuffer, 4u, 100u);
   HAL_GPIO_WritePin(CS_FLASH_GPIO_Port, CS_FLASH_Pin, GPIO_PIN_SET);
+  // Check ID
+  if( (FLASH_RxBuffer[1u] == 0xEFu) &&  (FLASH_RxBuffer[2u] == 0x40u) && (FLASH_RxBuffer[3u] == 0x16u) )
+  {
+    FLASH_Detected = 1u;
+  }
 }
 
 
