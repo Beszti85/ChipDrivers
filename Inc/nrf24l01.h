8350ea4b108d563b25fdc887deb4ab8edb36ed92
Beszti85 <51108540+Beszti85@users.noreply.github.com>
2022-07-10 09:25:48 +0200

    Add NRF24L01 chipdriver

---
+++ Inc/nrf24l01.h
@@ -0,0 +1,13 @@
+/*
+ * nrf24l01.h
+ *
+ *  Created on: 2022. júl. 6.
+ *      Author: drCsabesz
+ */
+
+#ifndef INC_NRF24L01_H_
+#define INC_NRF24L01_H_
+
+void NRF24L01_Init( void );
+
+#endif /* INC_NRF24L01_H_ */
