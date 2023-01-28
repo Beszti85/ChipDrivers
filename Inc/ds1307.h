3006e315144a4cefcc891e30aea5e8ced1c64fec
Beszti85 <csababesztercei@gmail.com>
2023-01-28 15:21:57 +0100

    [DS1307]: initial driver version

---
+++ Inc/ds1307.h
@@ -0,0 +1,83 @@
+/*
+ * mcp23s17_spi.h
+ *
+ *  Created on: 2022. jún. 20.
+ *      Author: drCsabesz
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "main.h"
+
+#ifndef INC_DS1307_H_
+#define INC_DS1307_H_
+
+/* ADDRESS: 0xD0 */
+
+#define DS1307ADDR_R 0xD1
+#define DS1307ADDR_W 0xD0
+
+#define REG_SECONDS 0x00
+#define REG_MINUTES 0x01
+#define REG_HOURS   0x02
+#define REG_DAY     0x03
+#define REG_DATE    0x04
+#define REG_MONTH   0x05
+#define REG_YEAR    0x06
+#define REG_CONTROL 0x07
+
+#define RAM_START   0x09
+#define RAM_END     0x3F
+
+#define RAM_SIZE    56
+
+typedef struct
+{
+  I2C_HandleTypeDef* ptrHI2c;
+  uint8_t TxBuffer[56u];
+  uint8_t RxBuffer[56u];
+  uint8_t Address;
+  uint8_t Available;
+  uint8_t Padding[2u];
+} DS1307_Handler_t;
+
+typedef struct
+{
+  uint8_t Seconds;
+  uint8_t Minutes;
+  uint8_t Hours;
+  uint8_t Day;
+  uint8_t Date;
+  uint8_t Month;
+  uint8_t Year;
+} DS1307_TimeDate_t;
+
+typedef enum
+{
+  Freq1Hz = 0,
+  Freq4096Hz,
+  Freq8192Hz,
+  Freq32768Hz
+} DS1307_Frequency_e;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void DS1307_Init( DS1307_Handler_t* ptrHandler, DS1307_TimeDate_t* ptrCurrTime );
+//void DS1307_Set_Date ( DS1307_Handler_t* ptrHandler );
+void DS1307_SquareWaveOutput( DS1307_Handler_t* ptrHandler, DS1307_Frequency_e frequency );
+void DS1307_Read_Date( DS1307_Handler_t* ptrHandler, uint8_t *day, uint8_t *date, uint8_t *month, uint8_t *year);
+void DS1307_Read_Time( DS1307_Handler_t* ptrHandler, uint8_t *sec, uint8_t *min, uint8_t *hour);
+void DS1307_Convert_Time(char *s, uint8_t hour, uint8_t min, uint8_t sec);
+void DS1307_Convert_Date(char *s, uint8_t year, uint8_t month, uint8_t date);
+
+void DS1307_ROM_WriteByte( DS1307_Handler_t* ptrHandler, uint8_t address, uint8_t data);
+uint8_t DS1307_ROM_ReadByte( DS1307_Handler_t* ptrHandler, uint8_t address);
+void DS1307_Set_Out( DS1307_Handler_t* ptrHandler, uint8_t value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_DS1307_H_ */
---
+++ Src/ds1307.c
@@ -0,0 +1,124 @@
+#include "ds1307.h"
+
+
+/* Init to fix value 181h 37  */
+/* 2014/01/28 Tuesday */
+void DS1307_Init ( DS1307_Handler_t* ptrHandler, DS1307_TimeDate_t* ptrCurrTime )
+{
+  ptrHandler->TxBuffer[0u] = REG_SECONDS;
+  ptrHandler->TxBuffer[1u] = ptrCurrTime->Seconds;
+  ptrHandler->TxBuffer[2u] = ptrCurrTime->Minutes;
+  ptrHandler->TxBuffer[3u] = ptrCurrTime->Hours;
+  ptrHandler->TxBuffer[4u] = ptrCurrTime->Day;
+  ptrHandler->TxBuffer[5u] = ptrCurrTime->Date;
+  ptrHandler->TxBuffer[6u] = ptrCurrTime->Month;
+  ptrHandler->TxBuffer[7u] = ptrCurrTime->Year;
+
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 8u, 100u);
+}
+
+#if 0
+/* Init to fix value 181h 37  */
+/* 2014/01/28 Tuesday */
+void DS1307_Set_Date ( DS1307_Handler_t* ptrHandler )
+{
+    I2CStart();
+    I2CWrite(DS1307ADDR_W);
+    I2CWrite(REG_DAY);
+    I2CWrite(0x06);
+    I2CWrite(0x12);
+    I2CWrite(0x04);
+    I2CWrite(0x14);
+    I2CWrite(0x00);
+    I2CStop();
+}
+#endif
+
+/* Allow 1 sec SQWE output */
+void DS1307_SquareWaveOutput( DS1307_Handler_t* ptrHandler, DS1307_Frequency_e frequency )
+{
+  ptrHandler->TxBuffer[0u] = REG_CONTROL;
+  ptrHandler->TxBuffer[1u] = 0x10u | (uint8_t)frequency;
+
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 2u, 100u);
+}
+
+void DS1307_Read_Date ( DS1307_Handler_t* ptrHandler, uint8_t *day, uint8_t *date, uint8_t *month, uint8_t *year )
+{
+	ptrHandler->TxBuffer[0u] = REG_DAY;
+	HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 1u, 100u);
+	HAL_I2C_Master_Receive(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 4u, 100u);
+
+	*day = ptrHandler->RxBuffer[0u];
+	*date = ptrHandler->RxBuffer[1u];
+	*month = ptrHandler->RxBuffer[2u];
+	*year = ptrHandler->RxBuffer[3u];
+}
+
+void DS1307_Read_Time ( DS1307_Handler_t* ptrHandler, uint8_t *sec, uint8_t *min, uint8_t *hour )
+{
+  ptrHandler->TxBuffer[0u] = REG_SECONDS;
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 1u, 100u);
+  HAL_I2C_Master_Receive(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 3u, 100u);
+
+  *sec = ptrHandler->RxBuffer[0u];
+  *min = ptrHandler->RxBuffer[1u];
+  *hour = ptrHandler->RxBuffer[2u];
+}
+
+void DS1307_Convert_Time (char *s, uint8_t hour, uint8_t min, uint8_t sec)
+{
+	/* fix size: 9 */ 
+	s[0] = ((hour >> 4) & 0x03) + '0';
+	s[1] = (hour & 0x0F) + '0';
+	s[2] = ':';
+	s[3] = (min >> 4) +'0';
+	s[4] = (min & 0x0F) + '0';
+	s[5] = ':';
+	s[6] = (sec >> 4) +'0';
+	s[7] = (sec & 0x0F) + '0';
+	s[8] = '\0';
+}
+
+void DS1307_Convert_Date(char *s, uint8_t year, uint8_t month, uint8_t date)
+{
+	/* fix size: 9 */
+	s[0] = '2';
+	s[1] = '0';
+	s[2] = ((year >> 4) & 0x0F) + '0';
+	s[3] = (year & 0x0F) + '0';
+	s[4] = '/';
+	s[5] = ((month >> 4) & 0x01)+'0';
+	s[6] = (month & 0x0F) + '0';
+	s[7] = '/';
+	s[8] = ((date >> 4) & 0x03) +'0';
+	s[9] = (date & 0x0F) + '0';
+	s[10] = '\0';
+}
+
+void DS1307_ROM_WriteByte( DS1307_Handler_t* ptrHandler, uint8_t address, uint8_t data )
+{
+  ptrHandler->TxBuffer[0u] = address;
+  ptrHandler->TxBuffer[1u] = data;
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 2u, 100u);
+}
+
+uint8_t DS1307_ROM_ReadByte( DS1307_Handler_t* ptrHandler, uint8_t address )
+{
+  ptrHandler->TxBuffer[0u] = address;
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 1u, 100u);
+  HAL_I2C_Master_Receive(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 1u, 100u);
+
+  return ptrHandler->RxBuffer[0u];
+}
+
+void DS1307_Set_Out( DS1307_Handler_t* ptrHandler, uint8_t value )
+{
+  ptrHandler->TxBuffer[0u] = REG_CONTROL;
+  ptrHandler->TxBuffer[1u] = ( value ? 0x80u : 0x00u );
+
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, 2u, 100u);
+}
+
+
+
