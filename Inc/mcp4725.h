0c44981f412d10724121398218b5653cb5846778
Beszti85 <csababesztercei@gmail.com>
2026-02-15 22:53:02 +0100

    [MCP4725]: fast mode added

--- Inc/mcp4725.h
+++ Inc/mcp4725.h
@@ -15,11 +15,18 @@ typedef struct
   I2C_HandleTypeDef* ptrHI2c;
   uint8_t TxBuffer[6u];
   uint8_t RxBuffer[6u];
+  uint16_t VddValue;   //!< Power supply value in 2 decimal points (x100)
   uint8_t Address;
-  uint8_t VddValue;
 } MCP4725_Handle_s;
 
-void MCP4725_Read(MCP4725_Handle_s* ptrHandler);
-void MCP4725_Write(MCP4725_Handle_s* ptrHandler);
+typedef enum
+{
+  MCP4725_FAST_MODE    = 0u,
+  MCP4725_WRITE_DAC    = 0x40u,
+  MCP4725_WRITE_DAC_EE = 0x60u
+} MCP4725_WriteCmd_e;
+
+void MCP4725_Read(MCP4725_Handle_s* ptrHandler, uint8_t length);
+void MCP4725_Write(MCP4725_Handle_s* ptrHandler, uint8_t length);
 
 #endif /* INC_MCP4725_H_ */
--- Src/mcp4725.c
+++ Src/mcp4725.c
@@ -13,15 +13,35 @@
 
 #define MCP4725_ADDR 0xC0u
 
-uint8_t MCP4725_RxBuffer[6u];
-uint8_t MCP4725_TxBuffer[6u];
-
-void MCP4725_Read(MCP4725_Handle_s* ptrHandler)
+void MCP4725_SetOutput(MCP4725_Handle_s* ptrHandler, uint8_t voltage, MCP4725_WriteCmd_e wrCmdType)
 {
-  HAL_I2C_Master_Receive(ptrHandler->ptrHI2c, MCP4725_ADDR, ptrHandler->RxBuffer, 6u, 100u);
+  uint16_t out_value = (uint16_t)((uint32_t)voltage * 4096u * 100u / ptrHandler->VddValue);
+
+  ptrHandler->TxBuffer[0] = wrCmdType;
+  switch (wrCmdType)
+  {
+    case MCP4725_FAST_MODE:
+      ptrHandler->TxBuffer[0u] |= (uint8_t)(out_value & 0xFF00u >> 8u);
+      ptrHandler->TxBuffer[1u] |= (uint8_t)(out_value & 0x00FFu);
+      ptrHandler->TxBuffer[2u] = ptrHandler->TxBuffer[0u];
+      ptrHandler->TxBuffer[3u] = ptrHandler->TxBuffer[1u];
+      MCP4725_Write(ptrHandler, 4u);
+      break;
+    case MCP4725_WRITE_DAC:
+      break;
+    case MCP4725_WRITE_DAC_EE:
+      break;
+    default:
+      break;
+  }
 }
 
-void MCP4725_Write(MCP4725_Handle_s* ptrHandler)
+void MCP4725_Read(MCP4725_Handle_s* ptrHandler, uint8_t length)
 {
-  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, MCP4725_ADDR, ptrHandler->TxBuffer, 6u, 100u);
+  HAL_I2C_Master_Receive(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->RxBuffer, length, 100u);
+}
+
+void MCP4725_Write(MCP4725_Handle_s* ptrHandler, uint8_t length)
+{
+  HAL_I2C_Master_Transmit(ptrHandler->ptrHI2c, ptrHandler->Address, ptrHandler->TxBuffer, length, 100u);
 }
